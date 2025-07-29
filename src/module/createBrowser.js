3d0a866a5992ce1b5e95d046335d7f872f3a8a16
KenshiDRK <KenshiDRK@gmail.com>
2025-07-29 19:14:07 +0200

    Update createBrowser.js

--- src/module/createBrowser.js
+++ src/module/createBrowser.js
@@ -9,6 +9,41 @@ async function createBrowser() {
 
         const { browser } = await connect({
             headless: false,
+            args: [
+                "--disable-dev-shm-usage",           // Evita uso intensivo de /dev/shm
+                "--disable-gpu",                     // Desactiva GPU (no necesaria en server)
+                "--disable-setuid-sandbox",          // Menos aislamiento, menos memoria
+                "--no-sandbox",                      // Desactiva sandboxing
+                "--no-zygote",                       // Desactiva procesos intermedios
+                "--disable-software-rasterizer",     // Evita carga de renderizado extra
+                "--disable-accelerated-2d-canvas",   // Menos carga en el renderizado
+                "--disable-dev-tools",               // DevTools innecesarios
+                "--no-first-run",                    // Evita configuraciones iniciales
+                "--mute-audio",                      // Silencia todo, menos carga
+                "--disable-background-networking",
+                "--disable-background-timer-throttling",
+                "--disable-breakpad",
+                "--disable-client-side-phishing-detection",
+                "--disable-default-apps",
+                "--disable-features=site-per-process",
+                "--disable-hang-monitor",
+                "--disable-popup-blocking",
+                "--disable-prompt-on-repost",
+                "--disable-renderer-backgrounding",
+                "--disable-sync",
+                "--metrics-recording-only",
+                "--no-default-browser-check",
+                "--safebrowsing-disable-auto-update",
+                "--password-store=basic",
+                "--use-mock-keychain",
+                "--disable-notifications",
+                "--disable-extensions",
+                "--hide-scrollbars",
+                //"--blink-settings=imagesEnabled=false", // ⚠️ Desactiva imágenes (hace fallar)
+                //"--enable-automation",...............// (hace fallar)
+                //"--single-process",                  // No forks (hace Fallar)
+                "--js-flags=--no-expose-wasm,--jitless", // Desactiva JIT y WebAssembly
+            ],
             turnstile: true,
             connectOption: { defaultViewport: null },
             disableXvfb: false,
